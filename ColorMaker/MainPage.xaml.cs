14d964ac34962b750c3c55bd004adbc2b6ab465e
Soner Paycı <sonerpayci@gmail.com>
2023-05-18 02:10:00 +0300

    Added Events and Event Handlers

--- ColorMaker/MainPage.xaml.cs
+++ ColorMaker/MainPage.xaml.cs
@@ -6,5 +6,30 @@ public partial class MainPage : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private void Slider_ValueChanged(object sender, ValueChangedEventArgs e)
+    {
+        var red = SliderRed.Value;
+        var green = SliderGreen.Value;
+        var blue = SliderBlue.Value;
+
+        var color = Color.FromRgb(red, green, blue);
+
+        SetColor(color);
+    }
+
+    private void SetColor(Color color)
+    {
+        BtnGenerate.BackgroundColor = color;
+        Container.BackgroundColor = color;
+        LabelHex.BackgroundColor = color;
+
+        LabelHex.Text = $"HEX Value : {color.ToHex()}";
+    }
+
+    private void BtnCopy_Clicked(object sender, EventArgs e)
+    {
+        Clipboard.Default.SetTextAsync(LabelHex.BackgroundColor.ToHex());
+    }
 }
 
